2ffa19ee717b06ecae742fc86c1032ee1ad0969b
Kim An Lim <11883170@student.uts.edu.au>
2017-08-26 23:21:23 +1000

    finish creating dialog layout

--- app/src/main/java/com/kasmartnotification/smartnotification/MainActivity.java
+++ app/src/main/java/com/kasmartnotification/smartnotification/MainActivity.java
@@ -1,15 +1,19 @@
 package com.kasmartnotification.smartnotification;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton smartNotiFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
         setSupportActionBar(toolbar);
 
+        smartNotiFab = findViewById(R.id.activity_main_fab_smartnotification);
+        smartNotiFab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
     }
 
     @Override
@@ -41,4 +52,11 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void showDialog(){
+        AppCompatDialog dialog = new AppCompatDialog(this);
+        dialog.setContentView(R.layout.smart_notification_dialog);
+        dialog.show();
+    }
+
 }
