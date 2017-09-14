c092b20991e14e3f528957021996e851467859d9
Aswin Hartono <aswinptr95@gmail.com>
2017-09-14 13:59:38 +1000

    Add settings views

---
+++ app/src/main/java/com/kasmartnotification/smartnotification/Controller/SettingsKeywordsResponse.java
@@ -0,0 +1,13 @@
+package com.kasmartnotification.smartnotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsKeywordsResponse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_keywords_response);
+    }
+}
---
+++ app/src/main/java/com/kasmartnotification/smartnotification/Controller/SettingsLocation.java
@@ -0,0 +1,13 @@
+package com.kasmartnotification.smartnotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_location);
+    }
+}
